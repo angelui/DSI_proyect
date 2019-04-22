f90e25baec0da3aab90ecae77fc2dad980b8d13a
Sabrii98 <100363834@alumnos.uc3m.es>
2019-04-22 15:55:42 +0200

    Nueva actualización
    Páginas de registro e inicio de sesión

---
+++ P2-ASOBOL/nuevoCodigo/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { InicioSesionPage } from '../pages/inicio-sesion/inicio-sesion';
+import { RegistroPage } from '../pages/registro/registro';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    InicioSesionPage,
+    RegistroPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    InicioSesionPage,
+    RegistroPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ P2-ASOBOL/nuevoCodigo/src/pages/home/home.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { InicioSesionPage } from '../inicio-sesion/inicio-sesion';
+import { RegistroPage } from '../registro/registro';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LibretaContactosPage');
+  }
+
+  InicioSesion(){
+  	this.navCtrl.push(InicioSesionPage);
+  }
+
+  Registro(){
+    this.navCtrl.push(RegistroPage);
+  }
+
+}
