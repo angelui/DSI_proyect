4d723c2f76ab255619846437850b3e3b41da3ee3
Sabrii98 <100363834@alumnos.uc3m.es>
2019-04-26 01:08:40 +0200

    Ajustes actualizados

--- P2-ASOBOL/nuevoCodigo/src/pages/ajustes/ajustes.ts
+++ P2-ASOBOL/nuevoCodigo/src/pages/ajustes/ajustes.ts
@@ -2,6 +2,19 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { NgModule } from '@angular/core';
 
+import { CerrarSesionPage } from '../cerrar-sesion/cerrar-sesion';
+import { CompartirPage } from '../compartir/compartir';
+import { ConocenosPage } from '../conocenos/conocenos';
+import { EditarPerfilPage } from '../editar-perfil/editar-perfil';
+import { ExportarDatosPage } from '../exportar-datos/exportar-datos';
+import { IdiomaPage } from '../idioma/idioma';
+import { MonedasNivelPage } from '../monedas-nivel/monedas-nivel';
+import { NotificacionesPage } from '../notificaciones/notificaciones';
+import { PrivacidadPage } from '../privacidad/privacidad';
+import { TemaPage } from '../tema/tema';
+import { TerminosCondicionesPage } from '../terminos-condiciones/terminos-condiciones';
+import { TipoUnidadPage } from '../tipo-unidad/tipo-unidad';
+
 /**
  * Generated class for the AjustesPage page.
  *
@@ -22,5 +35,56 @@ export class AjustesPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad AjustesPage');
   }
+
+  MostrarEditarPerfil(){
+    this.navCtrl.push(EditarPerfilPage);
+  }
+
+  MostrarTipoUnidad(){
+    this.navCtrl.push(TipoUnidadPage);
+  }
+
+  MostrarPrivacidad(){
+    this.navCtrl.push(PrivacidadPage);
+  }
+
+  MostrarNotificaciones(){
+    this.navCtrl.push(NotificacionesPage);
+  }
+  
+  MostrarTema(){
+    this.navCtrl.push(TemaPage);
+  }
+
+  MostrarIdioma(){
+    this.navCtrl.push(IdiomaPage);
+  }
+  
+  MostrarExportarDatos(){
+    this.navCtrl.push(ExportarDatosPage);
+  }
+  
+  MostrarMonedasNivel(){
+    this.navCtrl.push(MonedasNivelPage);
+  }
+  
+  MostrarTerminosCondiciones(){
+    this.navCtrl.push(TerminosCondicionesPage);
+  }
+  
+  MostrarCompartir(){
+    this.navCtrl.push(CompartirPage);
+  }
+
+  MostrarConocenos(){
+    this.navCtrl.push(ConocenosPage);
+  }
+  
+  CerrarSesion(){
+    this.navCtrl.push(CerrarSesionPage);
+  }
+
+
+
 
 }
