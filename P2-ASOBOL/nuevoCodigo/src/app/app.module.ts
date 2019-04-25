f5b97a96f15cd38d98088aa5f4b65a9f0364ae6b
Sabrii98 <100363834@alumnos.uc3m.es>
2019-04-26 01:22:05 +0200

    Actualizado cerrar sesión

--- P2-ASOBOL/nuevoCodigo/src/app/app.module.ts
+++ P2-ASOBOL/nuevoCodigo/src/app/app.module.ts
@@ -5,8 +5,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-
-
 import { AjustesPage } from '../pages/ajustes/ajustes';
 import { CerrarSesionPage } from '../pages/cerrar-sesion/cerrar-sesion';
 import { CompartirPage } from '../pages/compartir/compartir';
