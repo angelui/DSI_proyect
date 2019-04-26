2cc6512b79451f4a9fb8d5cc059ea011fe2e6d36
LauraYG <100363785@alumnos.uc3m.es>
2019-04-26 02:06:04 +0200

    Pagina Final

--- P2-ASOBOL/nuevoCodigo/src/pages/cerrar-sesion/cerrar-sesion.ts
+++ P2-ASOBOL/nuevoCodigo/src/pages/cerrar-sesion/cerrar-sesion.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {HomePage} from '../home/home';
 
 /**
  * Generated class for the CerrarSesionPage page.
@@ -22,4 +23,8 @@ export class CerrarSesionPage {
     console.log('ionViewDidLoad CerrarSesionPage');
   }
 
+  VolverInicio(){
+    this.navCtrl.push(HomePage);
+  }
 }
+
