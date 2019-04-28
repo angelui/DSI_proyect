6422500ac5fe93fc194234d7ae1851a122af6f5b
RaanGi <45555635+RaanGi@users.noreply.github.com>
2019-04-28 12:59:02 +0200

    Add 'unidades de pan'

--- P2-ASOBOL/nuevoCodigo/src/services/datos-service.ts
+++ P2-ASOBOL/nuevoCodigo/src/services/datos-service.ts
@@ -3,11 +3,17 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class DatosService{
-    public datos: Datos[] = [{"fecha":"1","hora":"1","azucar":1,"hemoglobina":1,"peso":1,"pulso":1,"sistolica":1,"diastolica":1,"notas":"njpwienvlejv\nkoevnwepov"},
-    {"fecha":"2","hora":"2","azucar":2,"hemoglobina":2,"peso":2,"pulso":2,"sistolica":2,"diastolica":2,"notas":"njpwienvlejv\nkoevnwepov"}];
+    
+    private datos: Datos[] = 
+    [{"fecha":"1","hora":"1","azucar":1,"hemoglobina":1,"ud_pan":1,"peso":1,"pulso":1,"sistolica":1,"diastolica":1,"notas":"njpwienvlejv\nkoevnwepov"},
+    {"fecha":"2","hora":"2","azucar":2,"hemoglobina":2,"ud_pan":2,"peso":2,"pulso":2,"sistolica":2,"diastolica":2,"notas":"njpwienvlejv\nkoevnwepov"}];
   
     constructor(){
         
+    }
+
+    addDato(value: Datos){
+        this.datos.push(value);
     }
 
     getDatos(){
