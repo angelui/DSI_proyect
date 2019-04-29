f5e2e4c3d66519d59f5db0d8082b5d6c0b1a5d99
Angel Luis <a>
2019-04-29 12:38:08 +0200

    Estadisticas

--- P2-ASOBOL/nuevoCodigo/src/pages/estadisticas/estadisticas.ts
+++ P2-ASOBOL/nuevoCodigo/src/pages/estadisticas/estadisticas.ts
@@ -78,4 +78,77 @@ export class EstadisticasPage {
     });
     return azucarArray;
   }
+
+  promedio(tiempo: String){
+    
+
+    if(tiempo = "Semana"){
+      this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+
+        type: 'line',
+        data: {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    label: "Glucemia",
+                    fill: false,
+                    lineTension: 0.2,
+                    backgroundColor: "rgba(75,192,192,0.4)",
+                    borderColor: "rgba(75,192,192,1)",
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: "rgba(75,192,192,1)",
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: this.getAzucar(this.datos),
+                    spanGaps: false,
+                }
+            ]
+        }
+      });
+    }
+
+    if(tiempo = "Mes"){
+      this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+
+        type: 'line',
+        data: {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    label: "Glucemia",
+                    fill: false,
+                    lineTension: 0.2,
+                    backgroundColor: "rgba(75,192,192,0.4)",
+                    borderColor: "rgba(75,192,192,1)",
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: "rgba(75,192,192,1)",
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: this.getAzucar(this.datos),
+                    spanGaps: false,
+                }
+            ]
+        }
+      });
+    }
+  }
+
 }
